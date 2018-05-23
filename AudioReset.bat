08f8c425ee96880bd27425f24e93aad3b709f618
Henry Nguyen <mocchimon@users.noreply.github.com>
2018-05-23 05:06:49 -0700

    Update AudioReset.bat

--- AudioReset.bat
+++ AudioReset.bat
@@ -1,4 +1,9 @@
-net stop audiosrv
-net stop AudioEndpointBuilder
-net start audiosrv
-net start AudioEndpointBuilder
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+
+START cmd.exe net /k "stop audiosrv"
+SLEEP 5
+START cmd.exe /k "net stop AudioEndpointBuilder"
+SLEEP 5
+START cmd.exe /k "net start audiosrv"
+SLEEP 5
+START cmd.exe /k "net start AudioEndpointBuilder"
