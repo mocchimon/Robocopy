1018bf87f562c6bb1e0d945a924b59aa97610e2d
Henry Nguyen <mocchimon@users.noreply.github.com>
2018-05-23 04:53:40 -0700

    Update better_enabletestsigning.bat

--- better_enabletestsigning.bat
+++ better_enabletestsigning.bat
@@ -2,24 +2,7 @@
 title Testsigning
 color 1b
 
-:: BatchGotAdmin
-:-------------------------------------
-REM  --> Check for permissions
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    "%temp%\getadmin.vbs"
-    exit /B
-:gotAdmin
-    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-    pushd "%CD%"
-    CD /D "%~dp0"
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 
 echo Testsigning by: Henry Nguyen
 echo :-------------------------------------
@@ -30,11 +13,11 @@ IF /I "%choice%"== "N" GOTO no
 if not '%choice%'==''
 ECHO "%choice%" is not valid, try again
 
-: execute
+:yes
 start cmd /k Bcdedit.exe /set TESTSIGNING ON
 goto END
 
-: executeNO
+:no
 start cmd /k Bcdedit.exe /set TESTSIGNING OFF
 goto END
 
