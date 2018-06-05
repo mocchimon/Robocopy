3400e174f6fa9654ef0f6f2a573747fe730549e1
Henry Nguyen <mocchimon@users.noreply.github.com>
2018-06-05 05:14:01 -0700

    Add files via upload

---
+++ WifiFix.bat
@@ -0,0 +1,27 @@
+@echo off
+title Testsigning
+color 1b
+
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+echo Testsigning by: Henry Nguyen
+echo :-------------------------------------
+:PROMPT
+SET /P choice=Do you wish to enable Testsigning (Y/N)? 
+IF /I "%choice%"== "Y" GOTO yes
+IF /I "%choice%"== "N" GOTO no
+if not '%choice%'==''
+ECHO "%choice%" is not valid, try again
+GOTO Prompt
+
+:yes
+START cmd.exe /k "netsh wlan set autoconfig enabled=yes interface="Wireless Network Connection" "
+goto END
+
+:no
+START cmd.exe /k "netsh wlan set autoconfig enabled=no interface="Wireless Network Connection" 
+goto END
+
+:END
+shutdown /r /c "Restarting computer to apply changes"
+pause
+exit
